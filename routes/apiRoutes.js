966536eded7dc18d0973e1cde3206682cb7e5448
HeatherAckerman <hla20115213@gmail.com>
2020-10-23 15:20:24 -0500

    Notes can now be saved

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,8 @@
-const fs = require("fs");
+
 const notes = require("../Develop/db/db.json")
+const fs = require("fs");
 const util = require("util");
+const writeFileAsync = util.promisify(fs.writeFile);
 
 
 
@@ -8,17 +10,27 @@ module.exports = function (app) {
 
     //GET /api/notes reads db.json and returns saved notes as JSON
     app.get("/api/notes", function (req, res) {
-     
+
+        res.json(notes)
+
     });
 
     //POST /api/notes receives new note, add to db.json, return new note
     //Create ID
-    app.post("/api/notes", function(req,res) {
+    app.post("/api/notes", function (req, res) {
 
+        let newNote = req.body;
+        console.log(newNote)
+
+        notes.push(newNote)
+        writeFileAsync("Develop/db/db.json", JSON.stringify(notes)).then(function () {
+            console.log(notes)
+        });
+        res.json(newNote);
     })
 
     //DELETE /api/notes/:id receive note id to be deleted, read notes and delete, then rewrite updated file
-    app.delete("/api/notes:id", function(req,res) {
+    app.delete("/api/notes:id", function (req, res) {
 
     })
 }

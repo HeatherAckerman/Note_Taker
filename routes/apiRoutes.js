ddbc0cb5d53b88486e59a8eefbf3f0c38b45c3ee
HeatherAckerman <hla20115213@gmail.com>
2020-10-23 15:51:39 -0500

    Notes have IDs!

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -17,10 +17,14 @@ module.exports = function (app) {
 
     //POST /api/notes receives new note, add to db.json, return new note
     //Create ID
+    
     app.post("/api/notes", function (req, res) {
-
         let newNote = req.body;
-        console.log(newNote)
+        let lastNoteID = notes[notes.length - 1]["ID"];
+        let newNoteID = lastNoteID + 1;
+        newNote["ID"] = newNoteID;
+        
+        console.log(newNote["ID"])
 
         notes.push(newNote)
         writeFileAsync("Develop/db/db.json", JSON.stringify(notes)).then(function () {
@@ -31,6 +35,7 @@ module.exports = function (app) {
 
     //DELETE /api/notes/:id receive note id to be deleted, read notes and delete, then rewrite updated file
     app.delete("/api/notes:id", function (req, res) {
+
 
     })
 }
